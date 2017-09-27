03e954ff4a2b7177a1c1af6d91fdbb4166630665
farkas <alfarks@gmail.com>
2017-09-27 17:42:27 -0300

    traduccion a español y modificacion de la pagina de inicio

--- tp_dssd/resources/views/welcome.blade.php
+++ tp_dssd/resources/views/welcome.blade.php
@@ -69,17 +69,17 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Inicio</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Inicio de sesión</a>
+                        <a href="{{ route('register') }}">Registrarse</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema de Gestión de Incidencias
                 </div>
 
                 <div class="links">
