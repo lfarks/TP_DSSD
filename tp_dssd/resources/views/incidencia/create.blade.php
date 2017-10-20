0680ac24d75b5d8e748439f56cd79a5303c2bcbc
farkas <alfarks@gmail.com>
2017-10-20 17:40:17 -0300

    mas cambios en la vista del formulario de incidencias

--- tp_dssd/resources/views/incidencia/create.blade.php
+++ tp_dssd/resources/views/incidencia/create.blade.php
@@ -24,34 +24,45 @@
               </ul>
           </div>
       @endif
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="panel panel-default">
+                      <div class="panel-heading">Formulario del incidente</div>
+                      <div class="panel-body">
+                        {{ Form::open(array('action' => 'IncidenciaController@store')) }}
+                        <div class="form-group col-sm-2">
+                          {!! Form::label('cliente', 'Cliente') !!}
+                          {!! Form::text('cliente', '', ['class' => 'form-control']) !!}
+                        </div>
 
-{{ Form::open(array('action' => 'IncidenciaController@store')) }}
-<div class="form-group col-sm-2">
-  {!! Form::label('cliente', 'Cliente') !!}
-  {!! Form::text('cliente', '', ['class' => 'form-control']) !!}
-</div>
+                        <div class="form-group col-sm-2">
+                          {!! Form::label('fecha', 'Fecha del incidente') !!}
+                          {!! Form::date('fecha', \Carbon\Carbon::now(), ['class' => 'form-control']); !!}
+                        </div>
 
-<div class="form-group col-sm-2">
-  {!! Form::label('fecha', 'Fecha del incidente') !!}
-  {!! Form::date('fecha', \Carbon\Carbon::now(), ['class' => 'form-control']); !!}
-</div>
+                        <div class="form-group col-sm-3">
+                          {!! Form::label('cantObj', 'Cantidad de objetos') !!}
+                          {!! Form::number('cantObj', '', ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group col-sm-6">
+                          {!! Form::label('desc', 'Descripcion') !!}
+                          {!! Form::textarea('desc', '', ['class' => 'form-control']) !!}
+                        </div>
 
-<div class="form-group col-sm-3">
-  {!! Form::label('cantObj', 'Cantidad de objetos') !!}
-  {!! Form::number('cantObj', '', ['class' => 'form-control']) !!}
-</div>
-<div class="form-group col-sm-6">
-  {!! Form::label('desc', 'Descripcion') !!}
-  {!! Form::textarea('desc', '', ['class' => 'form-control']) !!}
-</div>
-
-<div class="form-group col-sm-6">
-  {!! Form::label('motivo', 'Motivo') !!}
-  {!! Form::textarea('motivo', '', ['class' => 'form-control']) !!}
-</div>
-<div class="form-group col-sm-12">
-  {!! Form::submit('Enviar', ['class' => 'btn btn-success']) !!}
-  <!--<button class="btn btn-success" type="submit">Enviar</button>-->
-</div>
-{{ Form::close() }}
+                        <div class="form-group col-sm-6">
+                          {!! Form::label('motivo', 'Motivo') !!}
+                          {!! Form::textarea('motivo', '', ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group col-sm-12">
+                          {!! Form::submit('Enviar', ['class' => 'btn btn-success']) !!}
+                          <!--<button class="btn btn-success" type="submit">Enviar</button>-->
+                        </div>
+                        {{ Form::close() }}
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
 @endsection
